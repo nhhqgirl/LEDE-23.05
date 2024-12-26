e999c796778d7e7e654895bf550986aa136775af
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2024-12-26 22:33:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,16 +9,14 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# 移除要替换的包
-rm -rf feeds/packages/lang/golang
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/themes/luci-theme-argon
 
 # 1-添加 PowerOff 插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
 # 2-添加 Mosdns 插件
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
 # git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
 
 # 3-添加 Openclash 插件
@@ -28,4 +26,11 @@ cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-ap
 rm -rf package/openclash package/openclash.zip
 
 # 4-添加 argon 主题
+rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/sbwml/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+
+# 4-添加 samba4 插件
+rm -rf feeds/packages/libs/liburing
+git clone https://github.com/sbwml/feeds_packages_libs_liburing feeds/packages/libs/liburing
+rm -rf feeds/packages/net/samba4
+git clone https://github.com/sbwml/feeds_packages_net_samba4 feeds/packages/net/samba4
