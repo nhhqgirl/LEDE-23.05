439604976a08d52888208e680bee97d9cbeff275
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2024-12-29 13:13:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,17 +15,25 @@ rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/themes/luci-theme-argon
 
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+
+
+
+
 # 1-添加 PowerOff 插件
-git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
+# git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
 # 2-添加 Mosdns 插件
 # git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
 
 # 3-添加 Openclash 插件
-wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
-unzip -d package/openclash package/openclash.zip
-cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
-rm -rf package/openclash package/openclash.zip
+# wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
+# unzip -d package/openclash package/openclash.zip
+# cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+# rm -rf package/openclash package/openclash.zip
 
 # 4-添加 argon 主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
