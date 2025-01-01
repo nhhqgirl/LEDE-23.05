6c8dd865c78b3040cb54fe6d0b4caf2005974365
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-01 22:52:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,23 +10,38 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 1-添加 PowerOff 插件
-git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
-
-# 2-添加 Mosdns 插件
+# 移除要替换的包
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-docker
+rm -rf feeds/luci/applications/luci-app-dockerman
+
+
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+sed -i '$a src-git gxnas https://github.com/gxnas/OpenWrt_Build_x64_Packages' feeds.conf.default
+
+
+
+ 
+# 1-添加 PowerOff 插件
+# git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
+
+# 2-添加 Mosdns 插件
+# rm -rf feeds/packages/lang/golang
+# rm -rf feeds/packages/net/mosdns
+# rm -rf feeds/luci/applications/luci-app-mosdns
 # git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
 
 # 3-添加 Openclash 插件
-wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
-unzip -d package/openclash package/openclash.zip
-cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
-rm -rf package/openclash package/openclash.zip
+# wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
+# unzip -d package/openclash package/openclash.zip
+# cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+# rm -rf package/openclash package/openclash.zip
 
 # 4-添加 onliner 插件
-sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
+# sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
 
 # 5-添加  主题
 # rm -rf feeds/luci/themes/luci-theme-argon
