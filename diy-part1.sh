6a65b5f9265156390882266e4dae878ed02fa7ec
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-01 15:19:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,30 +10,23 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 移除要替换的包
+# 1-添加 Mosdns 插件
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/themes/luci-theme-argon
+git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
-
-
-sed -i '$a src-git leran https://github.com/217heidai/OpenWrt-Packages' feeds.conf.default
-
-
-
-
-
-# 1-添加 PowerOff 插件
-# git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
-
-# 2-添加 Mosdns 插件
-# git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
-
-# 3-添加 Openclash 插件
+# 2-添加 Openclash 插件
 wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
 unzip -d package/openclash package/openclash.zip
-cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+cp -r package/openclash/OpenClash-master/luci-app-openclash package/luci-app-openclash
 rm -rf package/openclash package/openclash.zip
 
-# 4-添加 argon 主题
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+# 3-添加 PowerOff 插件
+git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
+
+# 4-添加 onliner 插件
+sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
+
+
+# 5-添加 opentomcat 主题
+git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
