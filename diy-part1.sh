b04f3bed0bd4694fc3505c3bc901b76539a02fb6
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-02 20:14:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,18 +11,60 @@
 #
 
 # 移除旧包
-rm -rf feeds/packages/lang/golang/golang
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/rely/luci-app-openclash
 rm -rf feeds/xuanranran/luci-app-openclash
-rm -rf feeds/xuanranran/luci-app-poweroff
 
+# unzip
+rm -rf feeds/packages/utils/unzip
+git clone https://github.com/sbwml/feeds_packages_utils_unzip feeds/packages/utils/unzip
 
+# Update GCC 13.3.0
+rm -rf toolchain/gcc/*
+pushd toolchain/gcc/
+git clone --depth 1 https://github.com/immortalwrt/immortalwrt gcc && mv -n gcc/toolchain/gcc/* ./ ; rm -rf gcc
+popd
 
+# samba4 - bump version
+rm -rf feeds/packages/net/samba4
+git clone https://github.com/sbwml/feeds_packages_net_samba4 feeds/packages/net/samba4
+sed -i 's/ftp.gwdg.de/download.samba.org/g' feeds/packages/net/samba4/Makefile
+
+# liburing - 2.7 (samba-4.21.0)
+rm -rf feeds/packages/libs/liburing
+git clone https://github.com/sbwml/feeds_packages_libs_liburing feeds/packages/libs/liburing
+# enable multi-channel
+sed -i '/workgroup/a \\n\t## enable multi-channel' feeds/packages/net/samba4/files/smb.conf.template
+sed -i '/enable multi-channel/a \\tserver multi channel support = yes' feeds/packages/net/samba4/files/smb.conf.template
+# default config
+sed -i 's/#aio read size = 0/aio read size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#aio write size = 0/aio write size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/bind interfaces only = yes/bind interfaces only = no/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#create mask/create mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#directory mask/directory mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/0666/0644/g;s/0744/0755/g;s/0777/0755/g' feeds/luci/applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4.js
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/samba.config
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.template
+
+# Update node 20.x
+rm -rf feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
+
+# Update node-yarn
+rm -rf feeds/packages/lang/node-yarn/*
+pushd feeds/packages/lang/node-yarn/
+git clone --depth 1 https://github.com/immortalwrt/packages immortalwrt && mv -n immortalwrt/lang/node-yarn/* ./ ; rm -rf immortalwrt
+popd
+
+# Update golang 1.23.x
+rm -rf feeds/packages/lang/golang/golang
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang/golang
  
 # 添加 PowerOff 插件
+rm -rf feeds/xuanranran/luci-app-poweroff
 git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
 
 
