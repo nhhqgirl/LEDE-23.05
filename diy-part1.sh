f7be448fa0ab082e35bfcc864230949045004212
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-02 19:15:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,15 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+rm -rf feeds/xuanranran/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-openclash
+
+
+# Update golang 1.23.x
+rm -rf feeds/packages/lang/golang/golang
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang/golang
  
 # 1-添加 PowerOff 插件
 # git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
@@ -21,10 +30,10 @@
 # git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
 
 # 3-添加 Openclash 插件
-# wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
-# unzip -d package/openclash package/openclash.zip
-# cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
-# rm -rf package/openclash package/openclash.zip
+wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
+unzip -d package/openclash package/openclash.zip
+cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+rm -rf package/openclash package/openclash.zip
 
 # 4-添加 onliner 插件
 # sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
@@ -32,6 +41,6 @@
 # 5-添加  主题
 # rm -rf feeds/luci/themes/luci-theme-argon
 # git clone https://github.com/sbwml/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
+# git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
 
