3e4e2d6f2751828585948bb09c696caef756cf33
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-02 19:59:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,37 +10,21 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-rm -rf feeds/luci/themes/luci-theme-argon
+# 移除旧包
+rm -rf feeds/packages/lang/golang/golang
 rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf package/lean/{*ddns-scripts_aliyun,ddns-scripts_dnspod}
-rm -rf feeds/luci/applications/{luci-app-filebrowser,luci-app-argon-config}
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/packages/net/{*alist,chinadns-ng,dns2socks,dns2tcp,lucky,sing-box}
+rm -rf feeds/rely/luci-app-openclash
+rm -rf feeds/xuanranran/luci-app-openclash
+rm -rf feeds/xuanranran/luci-app-poweroff
 
 
 
  
-# 1-添加 PowerOff 插件
-# git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
+# 添加 PowerOff 插件
+git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
 
-# 2-添加 Mosdns 插件
-# rm -rf feeds/packages/net/mosdns
-# rm -rf feeds/luci/applications/luci-app-mosdns
-# git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
 
-# 3-添加 Openclash 插件
-wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
-unzip -d package/openclash package/openclash.zip
-cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
-rm -rf package/openclash package/openclash.zip
-
-# 4-添加 onliner 插件
-# sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
-
-# 5-添加  主题
-# rm -rf feeds/luci/themes/luci-theme-argon
-# git clone https://github.com/sbwml/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-# git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
 
