acad9e5497b78559ee872925e18f140b97155cca
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-03 11:32:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,7 +28,8 @@ cp -r package/openclash/OpenClash-master/luci-app-openclash package/luci-app-ope
 rm -rf package/openclash package/openclash.zip
 
 # 4-添加 onliner 插件
-# sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
+rm -rf feeds/xuanranran/luci-app-onliner
+sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
 
 
 # 5-添加 opentomcat 主题
