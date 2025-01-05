10354127aefa649c1fd8bbe16a78f8f1ef3d15c6
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-05 19:34:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,23 +14,22 @@ sed -i '$a src-git xuanranran https://github.com/xuanranran/openwrt-packages' fe
 sed -i '$a src-git rely https://github.com/xuanranran/rely' feeds.conf.default
 
 # 移除旧包
+rm -rf feeds/xuanranran/luci-app-onliner
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-dockerman
-
-# 添加 Openclash 插件
 rm -rf feeds/rely/luci-app-openclash
 rm -rf feeds/xuanranran/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
+
+# 添加 Openclash 插件
 wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
 unzip -d package/openclash package/openclash.zip
 cp -r package/openclash/OpenClash-master/luci-app-openclash package/luci-app-openclash
 rm -rf package/openclash package/openclash.zip
 
 # 4-添加 onliner 插件
-rm -rf feeds/xuanranran/luci-app-onliner
 sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
-
 
 # 5-添加 opentomcat 主题
 git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
