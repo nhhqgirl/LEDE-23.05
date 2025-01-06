67485d4aa43bccf4ef0cf5e68a8d2ad92431709c
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-06 10:59:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,8 +26,11 @@ unzip -d package/openclash package/openclash.zip
 cp -r package/openclash/OpenClash-master/luci-app-openclash package/luci-app-openclash
 rm -rf package/openclash package/openclash.zip
 
-# 4-添加 onliner 插件
-sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
+# 添加 onliner 插件
+#sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
 
-# 5-添加 opentomcat 主题
+# 3-添加 PowerOff 插件
+git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/luci-app-poweroff
+
+# 添加 opentomcat 主题
 git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
