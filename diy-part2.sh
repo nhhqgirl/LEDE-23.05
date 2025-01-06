be85bfa2efa71005e2b33963d8565653fb9c82ea
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-06 21:21:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,16 @@
 sed -i 's/192.168.1.1/192.168.81.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.81.1/g' package/base-files/luci2/bin/config_generate
 
+# 移除旧包
+rm -rf feeds/xuanranran/luci-app-onliner
+rm -rf feeds/xuanranran/luci-app-poweroff
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-dockerman
+rm -rf feeds/rely/luci-app-openclash
+rm -rf feeds/xuanranran/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+
 # Change default theme
 sed -i 's#luci-theme-bootstrap#luci-theme-opentomcat#g' feeds/luci/collections/luci/Makefile
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
