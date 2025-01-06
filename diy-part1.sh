a29405edca8428e2545485d9b8576031db5afdf1
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-06 22:13:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,14 +24,16 @@ rm -rf feeds/luci/applications/luci-app-openclash
 # 添加 Openclash 插件
 wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
 unzip -d package/openclash package/openclash.zip
-cp -r package/openclash/OpenClash-master/luci-app-openclash package/luci-app-openclash
-rm -rf package/openclash package/openclash.zip
+cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+rm -rf package/openclash
+rm -f package/openclash.zip
 
 # 添加 onliner 插件
-sed -i '$a src-git onliner https://github.com/nhhqgirl/luci-app-onliner' feeds.conf.default
+# sed -i '$a src-git onliner https://github.com/nhhqgirl/luci-app-onliner' feeds.conf.default
+git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
 
 # 3-添加 PowerOff 插件
-git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/luci-app-poweroff
+git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
 
 # 添加 opentomcat 主题
-git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
