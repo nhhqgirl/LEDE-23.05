705c5f1cb3c615db126d4d3ede9a1295d5cd84a4
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-08 19:29:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,9 +22,15 @@ rm -f package/openclash.zip
 git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
 
 # 添加 mosdns 插件
-# git clone https://github.com/nhhqgirl/luci-app-mosdns.git package/lean/luci-app-mosdns
-# git clone https://github.com/nhhqgirl/mosdns.git package/lean/mosdns
-# git clone https://github.com/nhhqgirl/v2dat.git package/lean/v2dat
+rm -rf feeds/luci/applications/luci-app-mosdns 
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2dat
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/nhhqgirl/luci-app-mosdns.git package/lean/luci-app-mosdns
+git clone https://github.com/nhhqgirl/mosdns.git package/lean/mosdns
+git clone https://github.com/nhhqgirl/v2dat.git package/lean/v2dat
+git clone https://github.com/nhhqgirl/v2ray-geodata.git package/lean/v2ray-geodata
+
 
 
 # 添加 PowerOff 插件
