47cde2bbdafdd9d33e8864ce706b203f62bff4c4
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-08 23:50:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,12 @@
+git clone --depth=1 https://github.com/xuanranran/openwrt-package customfeeds/lovepackages
+git clone --depth=1 https://github.com/xuanranran/rely customfeeds/loverely
+git clone --depth=1 https://github.com/immortalwrt/packages -b $REPO_BRANCH customfeeds/packages
+git clone --depth=1 https://github.com/immortalwrt/luci -b $REPO_BRANCH customfeeds/luci
+git clone --depth=1 https://github.com/openwrt/routing -b $REPO_BRANCH customfeeds/routing
+git clone --depth=1 https://github.com/openwrt/telephony -b $REPO_BRANCH customfeeds/telephony
+
+
+
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
