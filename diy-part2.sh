628993e2e4f4f0d29a6bf116b98f46687501d40d
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-11 00:26:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,8 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 # rm -rf feeds/luci/applications/luci-app-mosdns 
 # rm -rf feeds/packages/net/mosdns
