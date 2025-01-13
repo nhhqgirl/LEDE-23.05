5d79a1a05e4aa217555fd9c32c2dda82bd5a48cc
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-13 23:59:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -34,7 +34,7 @@ git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app
 
 
 # 添加 PowerOff 插件
-git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
+# git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
 
 # 添加 opentomcat 主题
-git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+# git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
