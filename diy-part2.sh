3facf75e5cae47a6ca63924ca8f47ef2a79aaa4d
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2025-01-14 09:29:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,6 @@
 #
 
 
-
-rm -rf feeds/luci/applications/luci-app-openclash
-
-
 # 修改网络设置
 sed -i 's/192.168.1.1/192.168.81.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.81.1/g' package/base-files/luci2/bin/config_generate
@@ -34,14 +30,3 @@ date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by LERAN/g" package/lean/default-settings/files/zzz-default-settings
 
-# 创建OpenClash使用的clash二进制文件所在的路径
-# mkdir -p files/etc/openclash/core
-# 下载[Meta] 当前内核版本
-#  CLASH_META_URL=$( curl -sL https://api.github.com/repos/vernesong/OpenClash/contents/dev/meta?ref=core | grep meta/clash-linux-amd64.tar.gz | awk -F '"' '{print $4}' | awk 'NR==4{print}' )
-# wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
-# 下载默认IP相关数据
-# wget -qO- https://cdn.jsdelivr.net/gh/alecthw/mmdb_china_ip_list@release/Country.mmdb > files/etc/openclash/Country.mmdb
-# wget -qO- https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat > files/etc/openclash/GeoIP.dat
-# wget -qO- https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat > files/etc/openclash/GeoSite.dat
-# 给Clash二进制文件增加执行权限
-# chmod +x files/etc/openclash/core/clash*
